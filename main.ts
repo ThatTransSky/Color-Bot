544f772181a8ec00f0ba876b62a8e74a7394714f
FNItsLegend <msurvival65@gmail.com>
2023-02-07 17:41:09 +0200

    no one cares

--- main.ts
+++ main.ts
@@ -21,34 +21,36 @@ let client = new Client({
 config();
 export type Client2 = Client & { commands: Collection<string, CommandStructure> };
 const client2 = client as Client2;
-/*
+(async () => {
+  /*
 TODO: Put the command collection and event handler in separate files to tidy up the code
 */
 
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+  const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
-const commands: any[] = [];
+  const commands: any[] = [];
 
-client2.commands = new Collection<string, CommandStructure>();
+  client2.commands = new Collection<string, CommandStructure>();
 
-for (const file of commandFiles) {
-  const command = (await import(`./commands/${file}`)) as CommandStructure;
-  // console.log(command);
-  commands.push(command.data);
-  client2.commands.set(command.data.name, command);
-}
-
-// Event Handler
-
-const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-  const event = await import(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, commands)); // Runs the "run-once" events.
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client)); // Runs the rest of the events.
+  for (const file of commandFiles) {
+    const command = (await import(`./commands/${file}`)) as CommandStructure;
+    // console.log(command);
+    commands.push(command.data);
+    client2.commands.set(command.data.name, command);
   }
-}
 
-client.login(process.env.TOKEN);
+  // Event Handler
+
+  const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const event = await import(`./events/${file}`);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, commands)); // Runs the "run-once" events.
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client)); // Runs the rest of the events.
+    }
+  }
+
+  client.login(process.env.TOKEN);
+})();
